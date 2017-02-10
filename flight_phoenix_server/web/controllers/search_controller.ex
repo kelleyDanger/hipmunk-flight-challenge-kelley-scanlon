11c479519eb5138e1dcd2225d4e5a264c36c892c
Kelley Scanlon <kelley@8tracks.com>
2017-02-09 20:34:52 -0800

    MUCH faster asynch API calls using Task.async and Task.wait

--- flight_phoenix_server/web/controllers/search_controller.ex
+++ flight_phoenix_server/web/controllers/search_controller.ex
@@ -4,20 +4,30 @@ defmodule HipmunkKelley.SearchController do
 
   def index(conn, _params) do
     providers = ["expedia", "orbitz", "priceline", "travelocity", "united"]
-    # Get all 6 responses in json form
-    results = Enum.flat_map(providers, fn(provider) ->
-      flight_results(provider)
-    end)
+    # Get all 5 responses in json form
+    # Ruby-like syntax
+    #results = Enum.flat_map(providers, fn(provider) ->
+    #  flight_results(provider)
+    #end)
+
+    # Elixir piped syntax
+   # results =
+   #   providers
+   #   |> Enum.flat_map(fn(provider) ->
+   #     flight_results(provider)
+   #   end)
+   #   |> Enum.sort_by(&Map.get(&1, "agony"))
+
 
     results =
       providers
-      |> Enum.flat_map(fn(provider) ->
-        flight_results(provider)
-      end)
-      |> Enum.sort_by(&Map.get(&1, "agony"))
+      |> Enum.map(fn(provider) ->
+        Task.async(fn -> flight_results(provider) end)
+      end) #mapping
+      |> Enum.map(fn(task) -> Task.await(task) end)
 
     conn
-    |> json(results)
+    |> json(%{results: results})
   end
 
   defp flight_results(provider) do
@@ -26,3 +36,20 @@ defmodule HipmunkKelley.SearchController do
     parsed_response["results"]
   end
 end
+
+
+
+# providers = ["expedia", "orbitz", "priceline", "travelocity", "united"]
+# res = []
+# providers.each do |provider|
+#  task = Task.async(fn -> flight_results(provider) end )
+#  result = Task.await(task)
+#  res << result
+# end
+#
+## RUBY
+# providers.map {|provider| provider + " airline"}
+#
+## ELIXIR
+# providers
+# |> Enum.map(fn(provider) -> provider + " airline" )
